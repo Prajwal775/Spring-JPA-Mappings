9e27762cbf7bdc7d6fce14539b75abffc0fdeb48
Prajwal Bharti <prajwalbharti7777@gmail.com>
2025-01-08 11:37:35 +0530

    Removed old folder 01-mappings-demo from the remote repository

--- 01-mappings-demo/src/main/java/com/mappings_demo/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mappings_demo.repository;
-
-import com.mappings_demo.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Integer> {
-}
--- 01-mappings-demo/src/main/java/com/mappings_demo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mappings_demo.repository;
-
-import com.mappings_demo.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
